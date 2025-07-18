062ea8ad233cc61b568882b459369c5d49d161d8
rfrancolini <rene.francolini@gmail.com>
2025-07-18 13:38:55 -0400

    initial docs

---
+++ R/sensible-package.R
@@ -0,0 +1,10 @@
+#' sensible
+#'
+#' This will process your hobo temperature data.
+#'
+#' @name sensible
+#' @aliases sensor processing and modeling
+#' @importFrom dplyr %>%
+#' @importFrom rlang .data
+#' @importFrom stats na.omit
+"_PACKAGE"
